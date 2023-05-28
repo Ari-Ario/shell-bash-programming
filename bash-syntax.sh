fa01d84ca60127d1f59c4426a93da29393a55470
Ari-Ario <xusrew.z@gmail.com>
2023-05-28 19:41:16 +0200

    syntax-file created

---
+++ bash-syntax.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+#first (above) line is obligatory, that refers to shell directory /bin/bash
+
+#printing a simple text:
+echo "text for test"
+
+#execute file just by writing ./filename. But the file must have execution mod:; write in terminal: chmod +x filename, then ./filename
