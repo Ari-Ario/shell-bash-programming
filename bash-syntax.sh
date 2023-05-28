0678d8c81684713b35a622590e63188ac2cb6b8a
Ari-Ario <xusrew.z@gmail.com>
2023-05-28 19:51:29 +0200

    user input update

--- bash-syntax.sh
+++ bash-syntax.sh
@@ -3,5 +3,9 @@
 
 #printing a simple text:
 echo "text for test"
-
 #execute file just by writing ./filename. But the file must have execution mod:; write in terminal: chmod +x filename, then ./filename
+
+#reading an input from user:
+echo "enter text, signs or numbers:"
+read input
+echo "your input is: $input"
