23e00e3f3fdb213b1a697b345dd071f887c67d7e
Ari-Ario <xusrew.z@gmail.com>
2023-05-29 20:45:00 +0200

    case block written

--- 2-case.sh
+++ 2-case.sh
@@ -2,3 +2,30 @@
 
 #case is a form of if-else but with diffrent syntax
 #a calculator which keeps track of calculations
+
+echo -e "Enter first number: \c"
+read num1
+
+echo -e "Enter operation from [+,-,*,/]: \c"
+read op
+
+echo -e "Enter second number: \c"
+read num2
+
+case $op in
+	"+" )
+		echo "$num1 + $num2" | bc
+	;;
+	"-" )
+		echo $"$num1 - $num2" | bc
+	;;
+	"*" )
+		echo $"$num1 * $num2" | bc
+	;;
+	"/" )
+		echo $"$num1 / $num2" | bc
+	;;
+	* )
+		echo "Not a valid operation $op"
+	;;
+esac
