1d608b80e5a7839852c62fecb0885ba9e85c9d43
Ari-Ario <xusrew.z@gmail.com>
2023-05-30 17:23:39 +0200

    body of while-loop writen

--- 3-while.sh
+++ 3-while.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 
 #explaining syntax with an example of reading text from a file
+
+echo -e "enter the file-path: \c"
+read path
+
+cat $path | while SLR= read -r line
+do
+	echo $line
+done
