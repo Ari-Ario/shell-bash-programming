01447ae1ea46569818922400dd289a2e7ce2e170
Ari-Ario <xusrew.z@gmail.com>
2023-06-04 09:07:29 +0200

    explanation expanded

--- 8-trap.sh
+++ 8-trap.sh
@@ -8,16 +8,20 @@
 #processing code to be traped from another terminal
 echo "pid of current processing code is: $$"
 
+#for example trap SIGINT everytime by: ctrl+c
+trap "echo "Exit caought"" SIGINT
+
 for (( i=0; i<=10; i++ ))
 do
     echo $i
     # numbers will be printed every 2 seconds to be able to trap script
     sleep 2
 done
-exit 0
+
+exit 0 #0 is success value
 
 #now you run this code in a terminal by command: ./8-tarp.sh
 #then open another terminal and write: kill -9 pid
 #most common ones: ctrl+c for iterupting or SIGINT, ctrl+z for stopping or suspending 
 
-#for a list of all signals in terminal: man 7 signal
+#for a list of all signals and their values in terminal: man 7 signal
