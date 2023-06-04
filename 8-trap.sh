09aca2c625de2d72a7fb27ee640f3d5c43fe9e3a
Ari-Ario <xusrew.z@gmail.com>
2023-06-04 08:49:15 +0200

    trap script and usage in different terminals

--- 8-trap.sh
+++ 8-trap.sh
@@ -3,5 +3,21 @@
 #trap command is used to catch and respond to signals or events in a script
 #there are several ways to use trap command
 
-#General syntax: trap command signals
+#General syntax given from terminal: trap command signals
 
+#processing code to be traped from another terminal
+echo "pid of current processing code is: $$"
+
+for (( i=0; i<=10; i++ ))
+do
+    echo $i
+    # numbers will be printed every 2 seconds to be able to trap script
+    sleep 2
+done
+exit 0
+
+#now you run this code in a terminal by command: ./8-tarp.sh
+#then open another terminal and write: kill -9 pid
+#most common ones: ctrl+c for iterupting or SIGINT, ctrl+z for stopping or suspending 
+
+#for a list of all signals in terminal: man 7 signal
