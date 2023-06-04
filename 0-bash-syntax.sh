07cfaa9fdfc1f49ac442591b71adf1b7596dc969
Ari-Ario <xusrew.z@gmail.com>
2023-06-04 18:22:05 +0200

    space of comments

--- 0-bash-syntax.sh
+++ 0-bash-syntax.sh
@@ -1,25 +1,25 @@
 #! /bin/bash
-#first (above) line is obligatory, that refers to shell directory /bin/bash
+# first (above) line is obligatory, that refers to shell directory /bin/bash
 
-#printing a simple text:
+# printing a simple text:
 echo "text for test"
-#execute file just by writing ./filename. But the file must have execution mod:; write in terminal: chmod +x filename, then ./filename
+# execute file just by writing ./filename. But the file must have execution mod:; write in terminal: chmod +x filename, then ./filename
 
-#reading an input from user:
+# reading an input from user:
 echo "enter text, signs or numbers:"
 read input
 echo "your input is: $input"
-#reading user-input on the same line:
+# reading user-input on the same line:
 echo -e "enter sth: \c"
 read sth
 echo "you input is: $sth"
 
-#Arithmetic calculations:
-#First for decimal numbers:
+# Arithmetic calculations:
+# First for decimal numbers:
 echo "first method of operations on decimal: $( expr 5 + 11 )"
-#or
+# or
 echo "second method for arithmetic operations: $(( 5 + 11 ))"
-#The same for all others except multiplication, which needs an extrta \*
+# The same for all others except multiplication, which needs an extrta \*
 echo "$( expr 5 - 11 )"
 echo "$(( 5 - 11 ))"
 echo "$( expr 5 \* 11 )"
@@ -29,55 +29,55 @@ echo "$(( 11 / 5 ))"
 echo "$( expr 11 % 5 )"
 echo "$(( 11 % 5 ))" #remainder
 
-#Arithmetic operations for float
+# Arithmetic operations for float
 echo " 1.2 + 2.3 " | bc
 echo " 2.3 - 1.2 " | bc
 echo " 1.2 * 2.3 " | bc
 echo " 2.3 / 1.2 " | bc
 
-#Arithmetic operations are the same with variables; for example:
+# Arithmetic operations are the same with variables; for example:
 echo -e "enter first number: \c"
 read num1
 echo -e "enter second number: \c"
 read num2
 echo "scale=2; $num1 / $num2" | bc -l #scale gives the number of floats back
 
-#array:
+# array:
 os=("Mac", "Linux", "Windows")
-#changing second element
+# changing second element
 os[1]="Ubuntu"
-#printing the second element
+# printing the second element
 echo "${os[1]}"
-#appending an element as 20th
+# appending an element as 20th
 os[20]="iOS"
-#printing all elements
+# printing all elements
 echo "${os[@]}"
-#printing the positions 
+# printing the positions 
 echo "${!os[@]}" #result is: 0 1 2 20
-#counting (number of) elements
+# counting (number of) elements
 echo "${#os[@]}"
 
 #readonly variables and functions, which cannot be overwritten, for example:
 var=10
 readonly var
-#var=11 has an error output
+# var=11 has an error output
 
 fun(){
     echo "inside a function"
 }
 readonly -f fun
-#fun(){ echo "sth"} has an error output too
+# fun(){ echo "sth"} has an error output too
 
-#pid of the running scrit and killing the execution from another termianl
+# pid of the running scrit and killing the execution from another termianl
 echo "pid is: $$"
 kill -9 #pid
 
 # push ctrl+c to terminate an execution and ctrl+z to stop it
 # push ctrl+d to terminate writting (cat) of a text
 
-#syntax of more complex programms is in other sections with examples
+# syntax of more complex programms is in other sections with examples
 
 
-#terminal-commands are very important, visit both websites:
-#https://explainshell.com
-#https://devdocs.io
+# terminal-commands are very important, visit both websites:
+# https://explainshell.com
+# https://devdocs.io
