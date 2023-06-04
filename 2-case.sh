e77c904440c221d8d1e65a046ea625bcfceb19c7
Ari-Ario <xusrew.z@gmail.com>
2023-06-04 20:15:38 +0200

    comm space

--- 2-case.sh
+++ 2-case.sh
@@ -1,24 +1,24 @@
 #! /bin/bash
 
-#case is a form of if-else but with diffrent syntax
-#a calculator which keeps track of calculations
+# case is a form of if-else but with diffrent syntax
+# a calculator which keeps track of calculations
 
-#readind first number from user-input as num1
+# readind first number from user-input as num1
 echo -e "Enter first number: \c"
 read num1
 
-#reading the operation sign from list [+,-,*,/] as user-input
+# reading the operation sign from list [+,-,*,/] as user-input
 echo -e "Enter operation from [+,-,*,/]: \c"
 read op
 
-#reading second number from user-input as num2
+# reading second number from user-input as num2
 echo -e "Enter second number: \c"
 read num2
 
-#printing the pre-calculation text and waiting for result on the same line
+# printing the pre-calculation text and waiting for result on the same line
 echo -e "the resulut of $num1 $op $num2 = \c"
 
-#case-block to operate the result
+# case-block to operate the result
 case $op in
 	"+" )
 		echo "$num1 + $num2" | bc
@@ -34,8 +34,8 @@ case $op in
 	;;
 	* )
 		echo "Not a valid operation $op. try again!"
-		 #this lin executes this file again, in case of an operation out of list [+,-,*,/]
+		 # this lin executes this file again, in case of an operation out of list [+,-,*,/]
 		"./"2-case.sh
 	;;
-#end of case-block with esac
+# end of case-block with esac
 esac
