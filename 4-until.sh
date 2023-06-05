07012fd2203278ade8c8757ca1e80bc607b25ade
Ari-Ario <xusrew.z@gmail.com>
2023-06-05 12:20:16 +0200

    comment-spacing

--- 4-until.sh
+++ 4-until.sh
@@ -1,24 +1,24 @@
 #! /bin/bash
 
-#until-loop is the revers form of while loop, therefore we write the same code-example of while-loop in until-loop
+# until-loop is the revers form of while loop, therefore we write the same code-example of while-loop in until-loop
 
-#symple syntax
+# symple syntax
 num=1
 until [ $num -ge 5 ] # or (( $num > 5 ))
 do
 	echo $num
-    #pre-encrementation diffrent from post-encrementation of while-loop example
+    # pre-encrementation diffrent from post-encrementation of while-loop example
     (( ++num ))
 done
 
-#more complex example of opening terminal multiple times; the same as while-loop
+# more complex example of opening terminal multiple times; the same as while-loop
 
-echo $num #it is incremented to 5
+echo $num # it is incremented to 5
 until (( $num > 10 ))
 do
 	echo $num
     (( num++ ))
-    #gnome-terminal is available on Linux; falls not, write an existing terminal in your OS
+    # gnome-terminal is available on Linux; falls not, write an existing terminal in your OS
     gnome-terminal &
-	#sleep 1
+	# sleep 1
 done
