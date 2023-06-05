6a3fcd166ee2b6635d5772912bb1efe51b677e8a
Ari-Ario <xusrew.z@gmail.com>
2023-06-05 12:25:50 +0200

    comment-spacing

--- 8-trap.sh
+++ 8-trap.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-#trap command is used to catch and respond to signals or events in a script
-#there are several ways to use trap command
+# trap command is used to catch and respond to signals or events in a script
+# there are several ways to use trap command
 
-#General syntax given from terminal: trap command signals
+# General syntax given from terminal: trap command signals
 
-#processing code to be traped from another terminal
+# processing code to be traped from another terminal
 echo "pid of current processing code is: $$"
 
-#for example trap SIGINT everytime by: ctrl+c
+# for example trap SIGINT everytime by: ctrl+c
 trap "echo "Exit caought"" SIGINT
 
 for (( i=0; i<=10; i++ ))
@@ -18,10 +18,10 @@ do
     sleep 2
 done
 
-exit 0 #0 is success value
+exit 0 # 0 is success value
 
-#now you run this code in a terminal by command: ./8-tarp.sh
-#then open another terminal and write: kill -9 pid
-#most common ones: ctrl+c for iterupting or SIGINT, ctrl+z for stopping or suspending 
+# now you run this code in a terminal by command: ./8-tarp.sh
+# then open another terminal and write: kill -9 pid
+# most common ones: ctrl+c for iterupting or SIGINT, ctrl+z for stopping or suspending 
 
-#for a list of all signals and their values in terminal: man 7 signal
+# for a list of all signals and their values in terminal: man 7 signal
