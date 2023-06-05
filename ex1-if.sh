3232a793ec5ab7b4dac19d7f578f0aa320757ac6
Ari-Ario <xusrew.z@gmail.com>
2023-06-05 20:00:27 +0200

    example 1 for if-else comprehension

---
+++ ex1-if.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# A simple calculator of if-else comprehension
+
+echo -e "Enter first number: \c"
+read num1
+
+echo -e "Enter the operation [+,-,*,/]: \c"
+read op
+
+echo -e "Enter second number: \c"
+read num2
+
+echo $num1, $op, $num2
