053be62590824d98b11759ce0b8dbcc4dbc63244
Ari-Ario <xusrew.z@gmail.com>
2023-06-05 21:56:12 +0200

    script finished

--- ex1-if.sh
+++ ex1-if.sh
@@ -5,14 +5,26 @@
 echo -e "Enter first number: \c"
 read num1
 
-echo -e "Enter the operation [+,-,*,/]: \c"
-read op
+set -f
+read -p "Enter the operation [+,-,*,/]: " op
+set +f
+echo $op
 
 echo -e "Enter second number: \c"
 read num2
 
 echo -e "$num1 $op $num2 = \c"
+
 if [ $op == "+" ]
 then
-    echo "$num1+$num2" |bc
+    echo "$num1+$num2" | bc
+elif [ $op == "-" ]
+then
+    echo "$num1-$num2" | bc
+elif [ $op == "*" ]
+then
+    echo "$num1*$num2" | bc
+elif [ $op == "/" ]
+then
+    echo "$num1/$num2" | bc -l
 fi
