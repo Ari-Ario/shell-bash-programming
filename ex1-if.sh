12bcfe95dacdb66be0e2c6e1f3bd6817b5b407e9
Ari-Ario <xusrew.z@gmail.com>
2023-06-06 07:47:55 +0200

    three examples to calculate, and to record the calculation in if-elif-else blocks

--- ex1-if.sh
+++ ex1-if.sh
@@ -16,7 +16,7 @@ read -p "Enter second number: " num2
 
 echo -e "$num1 $op $num2 = \c"
 
-# first and conventional method if-else
+# first and/or conventional method if-else
 if [ $op == "+" ]
 then
     echo "$num1+$num2" | bc
@@ -39,19 +39,30 @@ else
     fi
 fi
 
+# opening/creating a file in the same directory using if-elif
+if [ -f recorder.txt ]
+then
+    #cat >> cal-record.txt
+    echo -e "$num1 $op $num2 = \c" >> recorder.txt
+else
+    touch recorder.txt
+    #cat > cal-record.txt
+    echo -e "$num1 $op $num2 = \c" >| recorder.txt
+fi
+
 # second method using if-elif-else
 if [ $op == "+" ]
 then
-    echo "$num1+$num2" | bc
+    echo "$num1+$num2" | bc >> recorder.txt
 elif [ $op == "-" ]
 then
-    echo "$num1-$num2" | bc
+    echo "$num1-$num2" | bc >> recorder.txt
 elif [ $op == "*" ]
 then
-    echo "$num1*$num2" | bc
+    echo "$num1*$num2" | bc >> recorder.txt
 elif [ $op == "/" ]
 then
-    echo "$num1/$num2" | bc -l
+    echo "$num1/$num2" | bc -l >> recorder.txt
 else
     echo "$op is not included in [+.-,*,/]. Try again."
 fi
