adad78e42c1a068e8458cd39193067ad96f0221f
Ari-Ario <xusrew.z@gmail.com>
2023-06-09 08:42:18 +0200

    redirecting-file created, plus basic for and syntax

---
+++ ex4-redir.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# redirecting is the core of this file by saving output and taking input from other files
+
+function taking () {
+    :
+}
+
+function giving () {
+    :
+}
+
+# curly Brackets {} for the proceedur below
+{
+read -p "Enter keyword to search: " kw
+
+}
+exit 0
