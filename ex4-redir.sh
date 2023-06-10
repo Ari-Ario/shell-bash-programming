b029be2add90d0c9dd75b7fe77cf38ca1623e00d
Ari-Ario <xusrew.z@gmail.com>
2023-06-10 10:35:17 +0200

    functions and procedur finished

--- ex4-redir.sh
+++ ex4-redir.sh
@@ -1,18 +1,54 @@
 #!/bin/bash
 
-# redirecting is the core of this file by saving output and taking input from other files
+# I/O redirection if a file can be installed
 
-function taking () {
-    :
+function checking () {
+    if [ -z "$1" ]
+    then
+        echo "$1 is empty."
+        exit 0
+    else
+        # curly Brackets {} for input redirection
+        {
+            read line1
+        } < $1
+        echo "first line of the file:"
+        echo $line1
+    fi
 }
 
-function giving () {
-    :
+function installation () {
+    success=0
+    e_noargs=65
+    # curly Brackets {} for output redirection
+    {
+        echo "Archive Description:"
+        rmp -qpi $1
+        echo
+        echo "Archive Listing:"
+        rmp -qpl $1
+        echo
+        rmp -i --test $1
+        if [ "$?" -eq $success ]
+        then
+            echo "$1 can be installed."
+        else
+            echo "$1 cannot be installed!"
+        fi
+        echo
+    } > "$1.test"
+    echo "rmp-result in file $1.test"
 }
 
-# curly Brackets {} for the proceedur below
-{
-read -p "Enter keyword to search: " kw
+# This line passes the argument to checking, if it is empty or not
+checking $1
 
-}
+# This condition passes the arg to installation, if the file is executable
+if [ -x "$1" ]
+then 
+    installation $1
+else
+    echo "$1 has not executable mod."
+fi
+
 exit 0
